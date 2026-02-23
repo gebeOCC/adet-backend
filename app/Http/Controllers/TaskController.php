9f0380e668f2edd61742385d1f4b505317ff2d1b
gebeOCC <occ.gebe.barry@gmail.com>
2026-02-23 19:36:30 +0800

    modify task controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,18 +14,48 @@ class TaskController extends Controller
 
     public function store(Request $request)
     {
-        $task = Task::create($request->all());
-        return response()->json(['message' => 'success'], 201);
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string'
+        ]);
+
+        Task::create($validated);
+
+        return response()->json([
+            'message' => 'success'
+        ], 201);
     }
 
-    public function show(Task $task)
+    public function show($id)
     {
-        return $task;
+        $task = Task::find($id);
+
+        if (!$task) {
+            return response()->json([
+                'message' => 'Task not found'
+            ], 404);
+        }
+
+        return response()->json($task, 200);
     }
 
-    public function update(Request $request, Task $task)
+    public function update($id, Request $request)
     {
-        $task->update($request->all());
+        $task = Task::find($id);
+
+        if (!$task) {
+            return response()->json([
+                'message' => 'Task not found'
+            ], 404);
+        }
+
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string'
+        ]);
+
+        $task->update($validated);
+
         return response()->json($task, 200);
     }
 
